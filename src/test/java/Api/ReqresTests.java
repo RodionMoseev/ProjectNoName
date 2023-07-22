2b9fa6a3c041261bd0267c8aad2faa33dc7c95d6
Moseev Rodion <moseev_rodion@mail.ru>
2023-07-22 20:51:37 +0800

    wrote tests using the get method

--- src/test/java/Api/ReqresTests.java
+++ src/test/java/Api/ReqresTests.java
@@ -1,7 +1,7 @@
 package Api;
 
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.junit.Assert;
 
 import java.util.List;
 
@@ -13,11 +13,22 @@ public class ReqresTests {
   
   @Test
   public void checkAvatarAndIdTest(){
+    Specifications.installSpecification(Specifications.requestSpec(), Specifications.responseSpecOK200());
     List<UserData> users = given()
       .when()
-      .contentType(ContentType.JSON)
-      .get(URL+"api/users?page=2")
+      .get("api/users?page=2")
       .then().log().all()
-      .extract().body().jsonPath().getList("data",UserData.class);
+      .extract().body().jsonPath().getList("data", UserData.class);
+    users.forEach(x-> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
+    
+    Assert.assertTrue(users.stream().allMatch(x->x.getEmail().endsWith("@reqres.in")));
+    
+    List<String> avatars = users.stream().map(UserData::getAvatar).toList();
+  
+    List<String> ids = users.stream().map(x->x.getId().toString()).toList();
+    
+    for(int i = 0; i<avatars.size();i++){
+      Assert.assertTrue(avatars.get(i).contains(ids.get(i)));
+    }
   }
 }
